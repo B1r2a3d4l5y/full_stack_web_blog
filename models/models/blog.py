dda6958b44cf06dd02b2b297a7259716b2bb6d95
Bradley Wayne Arnold <82877822+B1r2a3d4l5y@users.noreply.github.com>
2021-11-16 08:21:09 +0200

    Update blog.py

--- models/models/blog.py
+++ models/models/blog.py
@@ -1,4 +1,4 @@
-from src.models.post import Post
+from post import Post
 import datetime
 
 import uuid
@@ -14,7 +14,7 @@ class Blog(object):
         self._id = uuid.uuid4().hex if _id is None else _id
 
     def new_post(self, title, content, date=datetime.datetime.utcnow()):
-        post = Post(blog_id=self.id,
+        post = Post(blog_id=self._id,
                     title=title,
                     content=content,
                     author=self.author,
@@ -22,7 +22,7 @@ class Blog(object):
         post.save_to_mongo()
 
     def get_posts(self):
-        return Post.from_blog(self.id)
+        return Post.from_blog(self._id)
 
     def save_to_mongo(self):
         Database.insert(collection='blogs',
@@ -40,7 +40,7 @@ class Blog(object):
     @classmethod
     def from_mongo(cls, id):
         blog_data = Database.find_one(collection='blogs',
-                                      query={'_id': _id})
+                                      query={'_id': id})
         return cls(**blog_data)
 
     @classmethod
