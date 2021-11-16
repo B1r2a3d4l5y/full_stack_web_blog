75306fe490b8719906a7213fefe98315e1450460
Bradley Wayne Arnold <82877822+B1r2a3d4l5y@users.noreply.github.com>
2021-11-16 08:23:11 +0200

    Update user.py

--- models/models/user.py
+++ models/models/user.py
@@ -1,59 +1,55 @@
 import uuid
 from flask import session
 from src.common.database import Database
-from src.models.blog import Blog
+from blog import Blog
 import datetime
 
 
 class User(object):
-    def __init__(self, email, password, _id):
+    def __init__(self, email, password, _id=None):
         self.email = email
         self.password = password
         self._id = self._id = uuid.uuid4().hex if _id is None else _id
 
+    @classmethod
+    def get_by_email(cls, email):
+        data = Database.find_one("users", {"email": email})
+        if data is not None:
+            return cls(**data)
 
-@classmethod
-def get_by_email(cls, email):
-    data = Database.find_one("users", {"email": email})
-    if data is not None:
-        return cls(**data)
+    @classmethod
+    def get_by_id(cls, _id):
+        data = Database.find_one("users", {"_id": _id})
+        if data is not None:
+            return cls(**data)
 
-
-@classmethod
-def get_by_id(cls, _id):
-    data = Database.find_one("users", {"_id": _id})
-    if data is not None:
-        return cls(**data)
-
-
-@staticmethod
-def login_valid(email, password):
-    # check weather a user's email matches the password they sent us
-    user = User.get_by_email(email)
-    if user is not None:
-        # check password
-        return user.password == password
-    return False
-
-
-@classmethod
-def register(cls, email, password):
-    user = cls.get_by_emai(email)
-    if user is None:
-        # user does not exits so create it
-        new_user = cls(email, password)
-        new_user.save_to_mongo()
-        session['email'] = email
-        return True
-    else:
-        # user exits
+    @staticmethod
+    def login_valid(email, password):
+        # check weather a user's email matches the password they sent us
+        user = User.get_by_email(email)
+        if user is not None:
+            # check password
+            return user.password == password
         return False
 
+    @classmethod
+    def register(cls, email, password):
+        user = cls.get_by_emai(email)
+        if user is None:
+            # user does not exits so create it
+            new_user = cls(email, password)
+            new_user.save_to_mongo()
+            session['email'] = email
+            return True
+        else:
+            # user exits
+            return False
 
-@staticmethod
-def login(user_email):
-    # Login_valid has already being called
-    session['email'] == user_email
+    @staticmethod
+    def login(user_email):
+        # Login_valid has already being called
+        session['email'] = user_email
+
 
 
 @staticmethod
@@ -63,7 +59,7 @@ def logout():
     def get_blogs(self, title, description):
         return Blog.find_by_author_id(self._id)
 
-    def new_blog(self):
+    def new_blog(self, title, description):
         blog = Blog(author=self.email,
                     title=title,
                     description=description,
@@ -76,7 +72,6 @@ def logout():
         blog.new_post(title=title,
                       content=content,
                       date=date)
-
 
     def json(self):
         return {
